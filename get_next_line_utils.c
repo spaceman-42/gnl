d86ded429ae10b7f51ae07bc2fceeae94b3313de
spaceman-42 <157530550+spaceman-42@users.noreply.github.com>
2025-07-12 17:00:32 +0200

    Update get_next_line_utils.c
    Added all prototypes from .h

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -11,3 +11,17 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	ft_bzero(void *s, size_t n);
+// to mesaure
+size_t	ft_strlen(const char *s);
+//to copy or get 
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strget(char *line);
+//to cut 
+char	*ft_strtrim(char const *s1, char const *set)
+// to join: concatenate two strings into a new string (with malloc).
+char	*ft_strjoin(char *s1, char *s2, int *locate);
+
