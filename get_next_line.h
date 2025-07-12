1d44ed50e7aa958c63c3ed076a2d1f89b3306a9d
spaceman-42 <157530550+spaceman-42@users.noreply.github.com>
2025-07-12 15:54:50 +0200

    Create get_next_line.h
    somedefinitios and notes on the libraries to use

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jadiaz-b <jadiaz-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 16:52:33 by jadiaz-b          #+#    #+#             */
+/*   Updated: 2025/02/25 18:12:27 by jadiaz-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <unistd.h>
+
+char *get_next_line(int fd)
+
+
+// Funtion name as per subject
+// Funtion prototipe as per char *get_next_line(int fd)
+// We can use read, malloc and free
+// malloc is in stdlib.h
+// free is in stdlib.h
