e5b45dbb3953f60053ae23ceb071fa705cef6d2f
spaceman-42 <157530550+spaceman-42@users.noreply.github.com>
2025-07-12 19:53:53 +0200

    Update get_next_line.c
    ft_strget lonking for a new line or the end of file

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,22 @@
 
 #include "get_next_line.h"
 
+//it takes a string an returs its length when reach '\n' or '\0'
+// menaing a new line or the end of file
 size_t	ft_strget(char *line)
+{
+  size_t i=0;
+  
+  if (!line)
+		return (-1);
+	while (i < BUFFER_SIZE)
+	{
+		if (line[i] == '\n' || line[i] == '\0')
+			return (i + 1);
+		i++;
+	}
+	return (i);
+}
 char	*read_line(char *line, int *length)
 char	*ft_newline(char *line, char *stash, int *locate, int fd)
 
