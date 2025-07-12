edf4b9801e3364a314b7d26013325b56bc23f2e3
spaceman-42 <157530550+spaceman-42@users.noreply.github.com>
2025-07-12 16:53:12 +0200

    Create get_next_line.c
    Functions to be in use

---
+++ get_next_line.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jadiaz-b <jadiaz-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 16:52:33 by jadiaz-b          #+#    #+#             */
+/*   Updated: 2025/02/25 18:12:27 by jadiaz-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strget(char *line)
+char	*read_line(char *line, int *length)
+char	*ft_newline(char *line, char *stash, int *locate, int fd)
+
+
+char	*get_next_line(int fd)
